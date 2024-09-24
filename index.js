a9ddfbe9066ac8cc2ddf110cd16b87fd5ebede0e
devarshi-balu <studious.devarshibalu@gmail.com>
2024-09-24 09:35:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// write a basic boiler plate code
+const express = require('express');
+const app = express() ;
+app.listen(3000);
+
+app.use(express.json());
+
+app.post('/todo', function(req, res) {
+
+})
+
+
+app.get('/todos', function(req, res) {
+
+})
+
+
+app.put('/completed', function(req, res) {
+    
+})
